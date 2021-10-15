7c046f4509ffd170ce50764ad46cbcc428a4a9d5
davidlew <12636609+mydavidlew@users.noreply.github.com>
2021-10-15 21:36:48 +0800

    Add files via upload

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
+#!/bin/sh
+#
 sudo apt update
 sudo apt list --upgradable
+sudo apt --fix-broken install -y
 sudo apt upgrade -y
 sudo apt dist-upgrade -y
-sudo apt autoremove
+sudo apt autoremove -y
