a79a23d685b1c9a5b7c9168230e8707e51650c11
davidlew <12636609+mydavidlew@users.noreply.github.com>
2021-10-27 08:48:13 +0800

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -4,5 +4,6 @@ sudo apt update
 sudo apt list --upgradable
 sudo apt --fix-broken install -y
 sudo apt upgrade -y
-sudo apt dist-upgrade -y
-sudo apt autoremove -y
+#sudo apt-get dist-upgrade -y
+#sudo apt autoremove -y
+#sudo apt-get clean && sudo apt-get purge -y $(dpkg -l | grep '^rc' | awk '{print $2}')
