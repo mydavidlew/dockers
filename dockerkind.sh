e1cbb4677d8dcb7aa40a351ce0737dd98eabf062
davidlew <12636609+mydavidlew@users.noreply.github.com>
2024-03-18 00:01:15 +0800

    Update dockerkind.sh

--- dockerkind.sh
+++ dockerkind.sh
@@ -236,5 +236,7 @@ elif [[ $# -eq 1 && $1 == $C_STOP ]] ; then
   kind delete cluster
 else
   echo "$(date) $line $$: You have gone to the wrong party! use '$0 $C_START/$C_STOP $APP_DASHBOARD/$APP_PORTAINER/$APP_INGRESS'"
+  # show the docker containers and systems
+  check_docker
 fi
 # end
