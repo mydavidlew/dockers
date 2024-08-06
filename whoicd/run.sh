84593a141bc32354bb381c04911e8c050c5418ca
davidlew <12636609+mydavidlew@users.noreply.github.com>
2024-08-06 23:11:49 +0800

    Add files via upload

--- whoicd/run.sh
+++ whoicd/run.sh
@@ -1,1 +1,1 @@
-docker run -it --rm --name=whoicd --network=appnet -e acceptLicense=true -e saveAnalytics=true -p 80:80 whoicd/icd-api
+docker run -it --rm --name=whoicd --network=appnet -e acceptLicense=true -e saveAnalytics=true -p 80:80 whoicd/icd-api:latest
