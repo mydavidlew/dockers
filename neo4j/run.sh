e6c0ffcdcdf65be991efaa05bb18f3c9a58e57a7
davidlew <12636609+mydavidlew@users.noreply.github.com>
2024-08-13 22:26:41 +0800

    Update run.sh

--- neo4j/run.sh
+++ neo4j/run.sh
@@ -1,12 +1,13 @@
 docker run -it --rm --name=neo4j --network=appnet --user="$(id -u):$(id -g)" -e NEO4J_AUTH=none -v $PWD/data:/data -p 7474:7474 -p 7687:7687 neo4j:latest
 
+# docker exec -it neo4j bin/cypher-shell -u neo4j -p neo4j
 
-#docker run -it --rm \
+# docker run -it --rm \
 #    --publish=7474:7474 --publish=7687:7687 \
 #    --volume=$HOME/neo4j/data:/data \
 #    neo4j:latest
 
-#docker run -it --rm \
+# docker run -it --rm \
 #  --publish=7474:7474 --publish=7687:7687 \
 #  --user="$(id -u):$(id -g)" \
 #  -e NEO4J_AUTH=none \
