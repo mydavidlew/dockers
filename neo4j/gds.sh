ccf628531202b80b3ceb9b0b75169978fdc9a66b
davidlew <12636609+mydavidlew@users.noreply.github.com>
2024-09-24 10:46:41 +0800

    Update gds.sh

--- neo4j/gds.sh
+++ neo4j/gds.sh
@@ -1,6 +1,5 @@
-docker run -it --rm \
+docker run -it --rm --name=neo4j --network=appnet --user="$(id -u):$(id -g)" -e NEO4J_AUTH=none \
   --publish=7474:7474 --publish=7687:7687 \
-  --user="$(id -u):$(id -g)" \
-  -e NEO4J_AUTH=none \
   --env NEO4J_PLUGINS='["graph-data-science"]' \
   neo4j:latest
+  
