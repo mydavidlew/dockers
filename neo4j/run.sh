339ec135d4ab5eaf0f34989728e24b7dcc257169
davidlew <12636609+mydavidlew@users.noreply.github.com>
2024-09-28 23:53:43 +0800

    Add files via upload

--- neo4j/run.sh
+++ neo4j/run.sh
@@ -1,10 +1,10 @@
-docker run -it --rm --name=neo4j --network=appnet --user="$(id -u):$(id -g)" -e NEO4J_AUTH=none -v $PWD/data:/data -p 7474:7474 -p 7687:7687 neo4j:latest
+docker run -it --rm --name=neo4j --network=appnet --user="$(id -u):$(id -g)" -e NEO4J_AUTH=none -v $PWD/data:/data -v $PWD/conf:/var/lib/neo4j/conf -p 7474:7474 -p 7687:7687 neo4j:latest
 
 # docker exec -it neo4j bin/cypher-shell -u neo4j -p neo4j
 
 # docker run -it --rm \
 #    --publish=7474:7474 --publish=7687:7687 \
-#    --volume=$HOME/neo4j/data:/data \
+#    --volume=$HOME/neo4j/data:/data --volumn=$PWD/conf:/var/lib/neo4j/conf \
 #    neo4j:latest
 
 # docker run -it --rm \
