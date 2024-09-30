9c609c01766434e5b97cdc5f661a04a9054a2837
davidlew <12636609+mydavidlew@users.noreply.github.com>
2024-09-30 17:31:59 +0800

    Add files via upload

--- neo4j/gds.sh
+++ neo4j/gds.sh
@@ -2,9 +2,25 @@ docker run -it --rm --name=neo4j --network=appnet --user="$(id -u):$(id -g)" -e 
   -v $PWD/data:/data -v $PWD/conf:/var/lib/neo4j/conf \
   --publish=7474:7474 --publish=7687:7687 \
   --env NEO4J_PLUGINS='["graph-data-science"]' \
+  --env NEO4J_apoc_export_file_enabled=true \
+  --env NEO4J_apoc_import_file_enabled=true \
+  --env NEO4J_apoc_import_file_use__neo4j__config=true \
+  --env NEO4J_PLUGINS='["apoc"]' \
   neo4j:latest
 
+# GDS Plugins
 #docker run -it --rm --name=neo4j --network=appnet --user="$(id -u):$(id -g)" -e NEO4J_AUTH=none \
+#  -v $PWD/data:/data -v $PWD/conf:/var/lib/neo4j/conf \
 #  --publish=7474:7474 --publish=7687:7687 \
 #  --env NEO4J_PLUGINS='["graph-data-science"]' \
 #  neo4j:latest
+
+# APOC Plugins
+#docker run \
+#    -p 7474:7474 -p 7687:7687 \
+#    --name neo4j-apoc \
+#    -e NEO4J_apoc_export_file_enabled=true \
+#    -e NEO4J_apoc_import_file_enabled=true \
+#    -e NEO4J_apoc_import_file_use__neo4j__config=true \
+#    -e NEO4J_PLUGINS=\[\"apoc\"\] \
+#    neo4j:5.24.0
--- neo4j/run.sh
+++ neo4j/run.sh
@@ -1,10 +1,13 @@
-docker run -it --rm --name=neo4j --network=appnet --user="$(id -u):$(id -g)" -e NEO4J_AUTH=none -v $PWD/data:/data -v $PWD/conf:/var/lib/neo4j/conf -p 7474:7474 -p 7687:7687 neo4j:latest
+docker run -it --rm --name=neo4j --network=appnet --user="$(id -u):$(id -g)" -e NEO4J_AUTH=none \
+  -v $PWD/data:/data -v $PWD/conf:/var/lib/neo4j/conf \
+  -p 7474:7474 -p 7687:7687 \
+  neo4j:latest
 
 # docker exec -it neo4j bin/cypher-shell -u neo4j -p neo4j
 
 # docker run -it --rm \
 #    --publish=7474:7474 --publish=7687:7687 \
-#    --volume=$HOME/neo4j/data:/data --volumn=$PWD/conf:/var/lib/neo4j/conf \
+#    --volume=$HOME/neo4j/data:/data \
 #    neo4j:latest
 
 # docker run -it --rm \
